910c6d30367c326561525f6327a42b5d5a151818
JlallyX <nastyj2003@gmail.com>
2023-11-22 00:25:56 +0200

    add style for section component

--- components/sections/sections.tsx
+++ components/sections/sections.tsx
@@ -3,19 +3,23 @@ import type SectionType from '../../interface/section';
 import Button from '../button';
 
 
-const Section: React.FC<SectionType> = ({ header, content }) => {
+const Section: React.FC<SectionType> = ({ header , content, image }) => {
   console.log(header)
   return (
     <div className='flex items-center justify-center'>
-      <div className='content-section flex items-center justify-center flex-col justify-center '>
+      <div className='content-section flex   items-start justify-center flex-col justify-center '>
 
-          <div className='flex items-center'>
-          <div className='flex justify-center flex-col'>{header}   </div>
+          <div className='flex items-center whitespace-normal'>
+          <div className='uppercase flex justify-center flex-col'>{header}   
+          {content}
+          </div>
     
-          {content }
+          {image }
           
         </div>
-        <Button />
+        <div className='absolute bottom-20 left-0'>
+        <Button/>
+        </div>
       </div>
     </div>
   );
--- interface/section.ts
+++ interface/section.ts
@@ -2,5 +2,6 @@ interface SectionData {
    content?: JSX.Element;
   header?: JSX.Element;
   button?: string;
+  image?: JSX.Element;
   }
 export default SectionData
