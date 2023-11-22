31933dabf33412f755bdc96202a8751ffb9796d1
JlallyX <nastyj2003@gmail.com>
2023-11-22 18:40:47 +0200

    Add styled section four

--- data/data-sections/section_four.tsx
+++ data/data-sections/section_four.tsx
@@ -2,7 +2,7 @@ import Button from "../../components/button";
 
 import ContentBenefits from '../../components/row-benefits/row-benefits'
 const header =
-  <h2>We <span>build </span>software to <br/> <strong> reach your goals</strong></h2>
+  <h2>Betis<span> build </span>dfasfasf asf asf s asf asfs</h2>
 
 
 const content = <ContentBenefits/>
