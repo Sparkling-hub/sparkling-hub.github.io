9d2b1699851fbbe45d03a2bdda713aa6b26da3bd
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-11-27 14:54:53 +0200

    add startUp page sectionOne

--- data/data-sections/data-sections-home/section_two.tsx
+++ data/data-sections/data-sections-home/section_two.tsx
@@ -3,7 +3,7 @@ import Services from "@/components/services"
 
 const header =
   <h1 className="flex items-center justify-center">
-    <span className="color-primary-light mx-4">We </span> <span className="text-black font-medium">Provide</span> <br />
+    <span className="color-primary-light mx-4">We </span> <span className="text-black font-black">Provide</span> <br />
   </h1>
 
 
