df528eac062984aaa25b901ede5571717da3f9f2
JlallyX <nastyj2003@gmail.com>
2023-11-28 13:42:16 +0200

    add section one in careers

---
+++ data/data-sections/data-section-careers/section_one.tsx
@@ -0,0 +1,18 @@
+import Button from "../../../components/button";
+
+const header =
+  <h1><strong className="s2">Careers</strong></h1>
+
+
+const content = <p>Sparkling Co. is Swiss headquartered company with presence all around Europe always in search of great professionals!
+   Want to join the team? Here are the current open positions.</p>
+
+const image = <img src='/img/section/sectionOne.png' className="w-full"/>;
+
+const element = {
+  content: content,
+  header: header,
+  image: image,
+
+}
+export default element
