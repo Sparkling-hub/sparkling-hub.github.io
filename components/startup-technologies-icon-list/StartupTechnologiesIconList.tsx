aef49a6665b3a6730beb0997bf98536d3b0f2c7e
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-11-29 18:03:25 +0200

    WEB-27 <add button>

--- components/startup-technologies-icon-list/StartupTechnologiesIconList.tsx
+++ components/startup-technologies-icon-list/StartupTechnologiesIconList.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import dataStartups from '@/data/data-sections/data-section-startup/data-startup-icons'
 import StartupIcon from '@/components/startup-icon';
+import Button from '../button';
 
 
 const StartupTechnologiesIconList: React.FC = () => {
 
 	return (
-		<div className='flex flex-wrap bg-emerald-900'>
+		<div className='flex flex-wrap justify-center items-center bg-emerald-900'>
 			{dataStartups.map((startup, index) => (
 				<StartupIcon key={index} {...startup}/>
 			))}
+			<div className='m-20'>
+				<Button href="/get-in-touch" text="Find out more" />
+			</div>
+			 
 		</div>
+
 
 	);
 };
