963e63e37b13c7156d3a0feb757de62a26ee4a7c
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-01 23:59:14 +0200

    WEB-00 <fix-footer svg 2 >

--- data/data-sections/data-sections-home/section_four.tsx
+++ data/data-sections/data-sections-home/section_four.tsx
@@ -8,6 +8,6 @@ const element = {
   content: content,
   header: header,
   id: "benefits", 
-  curvedClass: 'before:content-curved-primary-white before:w-full before:absolute -mt-20'
+  // curvedClass: 'before:content-curved-primary-white before:w-full before:absolute -mt-20'
 }
 export default element
