b0e695715214efad4054449cf3a4aae9b92a1acb
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-07 00:47:45 +0200

    Web 25 <start up section 1 - 1

--- data/data-sections/data-section-startup/data-startup-items/startupItemList4.tsx
+++
@@ -1,9 +0,0 @@
-const content = <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labasdasd dolore magna aliqua.</p>
-
-const header = <h3>A project manager</h3>
-
-const element = {
-    header: header,
-    content: content,
-}
-export default element
--- data/data-sections/data-section-startup/index.tsx
+++ data/data-sections/data-section-startup/index.tsx
@@ -1,12 +1,10 @@
 import SectionOne from "./section_one";
-import SectionTwo from "./section_two";
 
 import type  IStartupData  from "../../../interface/IStartupItemList";
 
 
 const sections: IStartupData[] = [
   SectionOne,
-  SectionTwo,
 ];
 
 export default sections
