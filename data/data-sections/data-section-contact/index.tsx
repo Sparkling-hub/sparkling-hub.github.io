3b1ae0e08eb518a4b8fb08b1639822dd607566da
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-06 11:11:51 +0200

    WEB-17 <fix smell bugs2>

--- data/data-sections/data-section-contact/index.tsx
+++ data/data-sections/data-section-contact/index.tsx
@@ -1,5 +1,4 @@
 import SectionOne from "./section_one";
-import SectionTwo from "./section_two";
 
 
 import type  SectionData  from "../../../interface/section";
@@ -8,9 +7,6 @@ import type  SectionData  from "../../../interface/section";
 
 export const sections: Record<string, SectionData> = {
   SectionOne: SectionOne,
-  SectionTwo:SectionTwo,
-
-
 };  
 
 
