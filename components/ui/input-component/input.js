97a311a99024a117ba47aba2d465aff078a4c3b0
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-06 10:35:09 +0200

    WEB-13 <fix smell bugs>

--- components/ui/input-component/input.js
+++
@@ -1,7 +0,0 @@
-const Input = ({ name, type, placeholder, value, onChange, id }) => {
-
-    return (
-        <input name={name} type={type} id = {id} placeholder={placeholder} value={value} onChange={onChange} className="border border-green-500 rounded-3xl p-4 w-full m-5" />
-    )
-}
-export default Input
