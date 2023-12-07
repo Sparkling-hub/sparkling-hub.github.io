fe43b52057ebe5a3191aba28cc94f7574fcc3020
JlallyX <nastyj2003@gmail.com>
2023-12-07 12:02:45 +0200

    Update section_two.tsx

--- data/data-sections/data-section-contact/section_two.tsx
+++ data/data-sections/data-section-contact/section_two.tsx
@@ -5,7 +5,7 @@ const header =
   <h3>Fill in the form below and <span>we will get back to you </span>within 24hours.</h3>
 
 
-const content = <><Form/></>
+const content = <Form/>
 
 
 const element = {
