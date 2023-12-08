ed5ec79375606e3431ade2356b841a2009cc1549
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-08 12:57:36 +0200

    Web 29 <fix bugs style>

--- data/data-sections/data-section-startup/section5.tsx
+++ data/data-sections/data-section-startup/section5.tsx
@@ -10,12 +10,11 @@ const header =
         <div className="flex justify-center text-5xl mb-4">
             
             <span className="color-primary-dark font-bold pb-5">Assisting you</span>
-            <span className="text-black font-light mx-4">all the way through</span>
+            <span className="text-black font-light mx-4 pb-20">all the way through</span>
         </div>
-        <div className="flex justify-center text-xl">
-            <span className="text-center text-black font-normal px-40 pb-20">While the MVP marks the starting line, it is not the finish line. It is the foundational groundwork imperative for a successful future. Our approach in developing an MVP keeps your future in mind and its stability as a core value ensuring a sound foundation that eases subsequent development.
-
-From this point onwards it is on you and your dream to lead the way, we offer our support and guidance, serving as your technological partner or consultant as you navigate the different stages of growth.</span>
+        <div className="flex-col justify-center text-xl">
+            <div className="text-center text-black font-normal px-40 pb-4">While the MVP marks the starting line, it is not the finish line. It is the foundational groundwork imperative for a successful future. Our approach in developing an MVP keeps your future in mind and its stability as a core value ensuring a sound foundation that eases subsequent development.</div>
+            <div className="text-center text-black font-normal px-40 pb-20">From this point onwards it is on you and your dream to lead the way, we offer our support and guidance, serving as your technological partner or consultant as you navigate the different stages of growth.</div>
         </div>
 
     </h2>
