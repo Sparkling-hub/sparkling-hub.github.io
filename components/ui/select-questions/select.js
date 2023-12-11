4ad2c7fb4f255f654c3dfc82df4845bf64e8aff8
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-11 15:51:49 +0200

    Web 29 <fix smells>

---
+++ components/ui/select-questions/select.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ButtonCircle from "../circle-button";
+
+const DisabledSelect = ({data}) => {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="relative m-5 ">
+      <div
+        className={`w-full p-3 border rounded cursor-pointer text-lg border-none ${
+          isDropdownOpen ? 'bg-emerald-900 text-teal-500 rounded-t-2xl' : 'rounded-2xl bg-slate-100'
+        }`}
+        onClick={handleDropdownToggle}
+        onKeyDown={handleDropdownToggle}
+      >
+
+        <span className="block relative flex justify-between ">{data.question}<ButtonCircle isDropdownOpen={isDropdownOpen}/> </span>
+
+        
+      </div>
+      {isDropdownOpen && (
+        <div className="top-full left-0 bg-white border border-gray-300 rounded-b-2xl p-4 w-full">
+          <p>{data.answer}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DisabledSelect;
