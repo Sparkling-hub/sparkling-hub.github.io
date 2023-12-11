e9ccd2a144db8d3a1a3bffebfb42dae94ca8ba73
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-11 14:41:48 +0200

    Web 27 <fix text>

--- data/data-sections/data-section-startup/data-startup-items/startupItemList5.tsx
+++ data/data-sections/data-section-startup/data-startup-items/startupItemList5.tsx
@@ -1,11 +1,11 @@
-const content = <p>focusses on enhancing user experience, creating modern and intuitive designs and prototypes to ensure user-friendliness.</p>
+const content = <p>Focusses on enhancing user experience, creating modern and intuitive designs and prototypes to ensure user-friendliness.</p>
 
 const header = <h3>A UI/UX Designer</h3>
 
 const element = {
     header: header,
     content: content,
-    img: '/img/startup/section2/UI_UX Designer.svg',
+    img: '/img/startup/section2/UI_UX Designer.png',
     index: 5,
 }
 export default element
