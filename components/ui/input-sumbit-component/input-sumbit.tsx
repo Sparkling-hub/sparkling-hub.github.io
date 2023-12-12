5058343d3dba60bd48bc5f1fafb69d886ace29d3
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-12 14:12:01 +0200

    Web 29 <fix smell>

--- components/ui/input-sumbit-component/input-sumbit.tsx
+++ components/ui/input-sumbit-component/input-sumbit.tsx
@@ -1,17 +1,17 @@
 import React, { SyntheticEvent } from 'react';
 import axios from "axios";
-import FormData from '@/interface/IFromData';
+
 interface InputSubmitProps {
   name: string;
   type: string;
-
-  
+  placeholder: string;
+  value: string;
   disabled: boolean;
-  formData: FormData;
+  formData: any;
   http: string;
 }
 
-const InputSubmit: React.FC<InputSubmitProps> = ({ name, type, disabled, formData, http }) => {
+const InputSubmit: React.FC<InputSubmitProps> = ({ name, type, placeholder, value, disabled, formData, http }) => {
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
@@ -22,14 +22,16 @@ const InputSubmit: React.FC<InputSubmitProps> = ({ name, type, disabled, formDat
       console.error('Send eeror:', error);
     }
   };
-  const buttonClass = disabled? 'bg-gradient-to-r from-teal-900 to-teal-300' : 'bg-color-primary-dark';
+  const buttonClass = disabled? 'bg-gradient-to-r from-teal-900 to-teal-300' : 'bg-teal-900';
 
   return (
     <input
       name={name}
       type={type}
+      placeholder={placeholder}
+      value={value}
       disabled={!disabled}
-      className={`no-underline text-white py-3 px-8  rounded-3xl p-2 w-40 m-auto ${buttonClass}`}
+      className={`no-underline text-white py-3 px-8  rounded-3xl p-2 w-40 ${buttonClass}`}
       onClick={handleSubmit}
     />
   );
--- components/ui/select-component/select.tsx
+++ components/ui/select-component/select.tsx
@@ -1,24 +1,30 @@
+import React, { ChangeEvent } from 'react';
 
 interface SelectProps {
   name: string;
-  value: string | undefined;
-  onChange: any;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
   placeholder: string;
-  data?: any;
+  data: any[]; // Замените any на конкретный тип вашего массива данных
 }
 
 const Select: React.FC<SelectProps> = ({ name, value, onChange, placeholder, data }) => {
   return (
     <select
       name={name}
-      className={`border border-green-500 rounded-3xl p-4 w-full my-5 ${value == '' ? "text-gray-400 " : ""}`}
-      value={value || ''}
+   
+      className="border border-green-500 rounded-3xl p-4 w-full m-5"
+      value={placeholder}
       onChange={onChange}
     >
-      <option value={placeholder}>
+      <option value= {placeholder}>
         {placeholder}
       </option>
-
+      {/* {data.map((item, index) => (
+        <option key={index} value={item.value}>
+          {item.label}
+        </option>
+      ))} */}
     </select>
   );
 };
