68cc90a033611e3913bdec39eb6bb7d456ac3cf5
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-13 14:30:59 +0200

    Web-26 <fix content section 2 page startup>

--- data/data-sections/data-section-startup/data-startup-items/startupItemList1.tsx
+++ data/data-sections/data-section-startup/data-startup-items/startupItemList1.tsx
@@ -2,7 +2,7 @@
 
 const content = <p>Provides crucial technical guidance, leading the project development, and ensuring control over the architecture and its related processes.</p>
 
-const header = <h3>A CTO or Technical Supervisor</h3>
+const header = <h3>CTO or Technical Supervisor</h3>
 
 const element = {
     header: header,
--- data/data-sections/data-section-startup/data-startup-items/startupItemList3.tsx
+++ data/data-sections/data-section-startup/data-startup-items/startupItemList3.tsx
@@ -1,11 +1,12 @@
-const content = <p>Handpicked on merit, infuse energy and innovation into projects, boosting the development progress (subject to timing and availability).</p>
+const content = <p>Ideally a collaborative team of minimum 2-3 engineers, working together to plan, execute and implement the code in Agile sprints.</p>
 
-const header = <h3>A team of Graduating Students</h3>
+const header = <h3>Team of Software Engineers</h3>
 
 const element = {
     header: header,
     content: content,
-    img: '/img/startup/section2/Students Avatar.svg',
+    img: '/img/startup/section2/Software Engineer Avatar.svg',
     index: 3,
+
 }
 export default element
