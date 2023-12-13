291e9411b5b2f82836e25c38c0a4921eb8d5e1b5
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-13 14:17:38 +0200

    Web 29 <fix style section 5 page startup>

--- components/assisting/Assisting.tsx
+++ components/assisting/Assisting.tsx
@@ -4,24 +4,26 @@ import React from 'react';
 import IStartupAssisting from '../../interface/IStartupAssisting'
 
 
-const Assisting: React.FC<IStartupAssisting> = ({ header, content, image, border, reverse, rotate }) => {
+const Assisting: React.FC<IStartupAssisting> = ({ header, content, image, background, reverse, rotate }) => {
 
 	
 
 
 
 	return (
-		<div className={`flex ${border} justify-between items-center ${!reverse ? 'flex-row-reverse' : ''}  m-10 rounded-4xl mx-32`}>
+		<div className={`flex ${background} justify-between items-center ${reverse ? 'flex-row-reverse' : ''}  m-10 rounded-4xl mx-32`}>
 
-			<div className='basis-5/6 flex-col justify-center  p-4'>
+			<div className='basis-5/6 flex-col justify-center mx-4 p-4'>
 				<div className='flex my-2'>
 					
-					<div className='text-emerald-900 font-bold'>{header}</div>
+					<div className='mx-4 text-emerald-900 font-bold'>{header}</div>
 				</div>
 				<div className='mx-4 my-2'>{content}</div>
 				
 			</div>
-			<div className={`${rotate}`}>{image}</div>
+			<div className={`${rotate}`}>
+			<img className='w-60 h-60 inline-block mx-4' src={image} alt=""/>
+				</div>
 		</div>
 	);
 };
--- data/data-sections/data-section-startup/data-startup-assistings/startupAssisting2.tsx
+++ data/data-sections/data-section-startup/data-startup-assistings/startupAssisting2.tsx
@@ -4,14 +4,14 @@ const header = <h3>Learn and Grow</h3>
 
 const content = <p>We are here to sustain you in your growth! In Continuing your journey, whether by refining your product, integrating new features, or exploring further R&D for new products, we stand by to assist. Our team is equipped to guide you through these crucial stages, helping you achieve your objectives.</p>
 
-const image = <img className='w-40 h-40 inline-block mx-4' src='/img/startup/assisting-plane/medium_plane.png' alt=""/>
+const image = '/img/startup/assisting-plane/medium_plane.png'
 
 const element = {
     header: header,
     content: content,
     image: image,
     rotate: 'transform scale-x-[-1]',
-    border: 'border-primary-light-light',
+    background: 'bg-primary-light-light',
     index: 2, 
 
 }
