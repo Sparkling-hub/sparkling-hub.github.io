6e9a99458a3ae86488061224671f6113eab5236d
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-18 18:54:09 +0200

    WEB-24 <fix smell_8>

--- components/multi-select-active-item/multi-select-active-item.tsx
+++ components/multi-select-active-item/multi-select-active-item.tsx
@@ -6,7 +6,18 @@ const MultiSelectActiveItem: React.FC<IMultiSelectActiveItem> = ({ id, deleteIte
 
 		<div className="flex flex-no-wrap active-item-container" title={id} >
 			<button type="button" className="" title="Remove item" aria-label="Remove item" aria-describedby="select2-filter_locations-container-choice-rjty-Tel Aviv">
-				<span className='delete_item_button' id={id} aria-hidden="true" onClick={deleteItem} onKeyDown={deleteItem}>×</span>
+				<span 	id={id} 
+						className='delete_item_button'  
+						aria-hidden="true" 
+						onClick={deleteItem} 
+						onKeyDown={(event: React.KeyboardEvent<HTMLDivElement>) => {
+							if (event.key === 'Enter' || event.key === ' ') {
+								deleteItem;
+							}
+						  }}>
+
+							×
+				</span>
 			</button>
 			<span className="item-title select2-selection__choice__display" >{id}</span>
 		</div>
