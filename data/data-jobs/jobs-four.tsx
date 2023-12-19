4ca3b8840c6ac313bbe54199978de53324f65da7
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-19 18:29:22 +0200

    WEB-24 <fix smell_20>

--- data/data-jobs/jobs-four.tsx
+++ data/data-jobs/jobs-four.tsx
@@ -1,41 +1,41 @@
 const experience = <p>PAID POSITION</p>
-const head = <h3>Full stack - Node JS - React JS Intern</h3>
+const head = <h3>Full stack - Node JS - React JS Intern1</h3>
 
 const text = <p>
 
-        Full time or part time<br />
-        Monthly salary depending on experience</p>
+        Full time or part time1<br />
+        Monthly salary depending on experience1</p>
 const location = <> <img src="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/flags/1x1/tn.svg" height={13} alt="" /><p>Tunis</p></>
 
 
 const jobs = {
     id: "Tunis",
-    namePosition: "Full stack - Noe JS - Reat JS Intrn",
+    namePosition: "Full stack - Noe JS - Reat JS Intrn1",
     location:location,
     experience: experience,
     head: head,
     text: text,
-    slug: 'php-dev-lugpart',
-    task: ['Backend development in Magnto or WordPress.'],
+    slug: 'php-dev-lugpart1',
+    task: ['Backend development in Magnto or WordPress.1'],
     nameProf: 'PHP DEVELPERS',
     descript:<p>We are looking for PHP developers to introduce on a team focused on Magento or WordPress.</p>,
     mustHave: [
-        'Completed a deg in computer science (or similar)',
-        '1-3 ars experience on similar role',
-        'Relevantnd CV-proven experience in PHP projects',
-        'Good knowlge in PHP',
-        'Knowlee in Laravel / Zend frameworks',
-        'Knowlge in English spoken and written is mdatory.'
+        'Complr)',
+        '1-3 ars role',
+        'Rence in PHP projects',
+        'GoodP',
+        'Knowleemeworks',
+        'Knowlge in is mdatory.'
     ],
     niceToHave: [
-        'Knowledge in Mento 1 and/or Magento 2',
-        'Knowledge in Wordess',
-        'Experience in Ale environment',
-        'Knowledge in Treo, Jira, Confluence.'
+        'Knowledgegento 2',
+        'Kndess',
+        'Ale environment',
+        'KnoConfluence.'
     ], weOffer: [
-        'Career oppounities',
-        'English lesns',
-        'Chartered aountant for quarterly tax/consultancy.'
+        'nities',
+        'Ens',
+        'Charly tax/consultancy.'
     ]
 }
 export default jobs
--- data/data-jobs/jobs-two.tsx
+++ data/data-jobs/jobs-two.tsx
@@ -1,5 +1,5 @@
 const experience = <p>PAID POSITION</p>
-const head = <h3>Full stack - Node JS - React JS Intern</h3>
+const head = <h3>Full stack - Node JS - React JS Intern2</h3>
 
 const text = <p>
 
@@ -10,32 +10,32 @@ const location = <> <img src="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-c
 
 const jobs = {
     id: "Kyiv",
-    namePosition: "Full stack - Node JS - Rea JS Intern",
+    namePosition: "Full  JS - Rea JS Intern",
     location:location,
     experience: experience,
     head: head,
     text: text,
     slug: 'php-dev-lupart',
-    task: ['Backend devopment in Magento or WordPress.'],
+    task: ['Backend devgento or WordPress.'],
     nameProf: 'PHP DEVOPERS',
     descript:<p>We are looking for PHP developers to introduce on a team focused on Magento or WordPress.</p>,
     mustHave: [
-        'Completed a degree in couter science (or similar)',
+        'Completed r science (or similar)',
         '1-3 years expience on similar role',
-        'Relevant d CV-proven experience in PHP projects',
-        'Good knowledgen PHP',
-        'Knowledge in Larel / Zend frameworks',
-        'Knowlge in English spoken and written is mandatory.'
+        'Relevant dperience in PHP projects',
+        'Gooddgen PHP',
+        'Zend frameworks',
+        ' spoken and ry.'
     ],
     niceToHave: [
-        'Knowledge in Mageo 1 and/or Magento 2',
-        'Knowled in WordPress',
-        'Experience iAgile environment',
-        'Knowledge in Trello, Jira, Confluence.'
+        'Knowento 2',
+        'Knowss',
+        'Ex environment',
+        'Knowlnfluence.'
     ], weOffer: [
-        'Career oppounities',
-        'English lesns',
-        'Chartered accountt for quarterly tax/consultancy.'
+        'Caities',
+        'Enns',
+        'Chartesultancy.'
     ]
 }
 export default jobs
