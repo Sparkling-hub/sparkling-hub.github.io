41a983dedd58de472f786786869d52333eb4d101
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-19 16:26:58 +0200

    WEB-24 <fix smell_10>

--- components/my-multi-select/my-multi-select.tsx
+++ components/my-multi-select/my-multi-select.tsx
@@ -8,18 +8,18 @@ const MyMultipleSelect: React.FC<IMultiSelect> = ({ name, uniqueIds, activeLocat
 
 	const [isActive, setIsActive] = useState(false);
 
-	const handleClick = (e: any) => {
+	const handleClick = () => {
 		setIsActive(!isActive);
 	};
-	const handleClickDropDown = (e: any) => {
+	const handleClickDropDown = (e: any) => {		
 		e.stopPropagation()
 	};
 	const [searchValue, setSearchValue] = useState('');
 	const [filteredIds, setFilteredIds] = useState(uniqueIds);
-	useEffect(() => {		
+	useEffect(() => {
 		setFilteredIds([...uniqueIds]);
 	}, [uniqueIds]);
-	
+
 	const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
 
 		setIsActive(true);
@@ -33,45 +33,46 @@ const MyMultipleSelect: React.FC<IMultiSelect> = ({ name, uniqueIds, activeLocat
 
 	return (
 		<div className={`my-multiple-select ${isActive ? 'active' : ''} m-4`} >
- 
-		<div className={`my-multiple-select-container flex-col flex rounded-lg relative bg-color-primary-medium border-bg-color-primary-medium`} 
- 			 role='button'
-			 onClick={handleClick} 
-			 onKeyDown={handleClick}   
-			 tabIndex={0}>
 
-			<div className={`${isActive ? 'min-h-8' : 'p-4'} flex left-3 flex-wrap items-center h-max ${activeLocations.length?' p-3':''}`}>
+			<div className={`my-multiple-select-container flex-col flex rounded-lg relative bg-color-primary-medium border-bg-color-primary-medium`}
+				role='button'
+				onClick={handleClick}
+				tabIndex={0}
+				onKeyDown={handleClick}
+			>
 
-				{activeLocations.map(id => (
-					<MultiSelectActiveItem key={id}
-						deleteItem={deleteItem}
+				<div className={`${isActive ? 'min-h-8' : 'p-4'} flex left-3 flex-wrap items-center h-max ${activeLocations.length ? ' p-3' : ''}`}>
+
+					{activeLocations.map(id => (
+						<MultiSelectActiveItem key={id}
+							deleteItem={deleteItem}
+							name={name}
+							activeLocations={activeLocations}
+							id={id} />
+					))}
+
+				</div>
+				<div className={` flex items-center absolute right-4 top-5 z-10`}><img src="/img/down-arrow-svgrepo-com.svg" className={`h-3 w-3 transition-transform transform ${isActive ? 'rotate-180' : 'rotate-0'}`} alt="" />
+				</div>
+
+				<div className={`mt-4`} >
+					<InputLocate
+						id={"focused_input"}
+						type={"text"}
+						value={searchValue}
+						placeholder={activeLocations.length ? "" : placeholder}
 						name={name}
-						activeLocations={activeLocations}
-						id={id} />
-				))}
+						handleSearchChange={handleSearchChange}
+					/><div className="flex top-19 h-max w-full left-0 list flex flex-wrap items-center p-4  rounded-b-lg border-t-[1px] border-slate-200 bg-color-primary-medium border-bg-color-primary-medium" onClick={handleClickDropDown}>
 
-			</div>
-			<div className={` flex items-center absolute right-4 top-5 z-10`}><img src="/img/down-arrow-svgrepo-com.svg" className={`h-3 w-3 transition-transform transform ${isActive ? 'rotate-180' : 'rotate-0'}`} alt="" />
-			</div>
-
-			<div className={`mt-4`} >
-				<InputLocate
-					id={"focused_input"}
-					type={"text"}
-					value={searchValue}
-					placeholder={activeLocations.length?"": placeholder }
-					name={name}
-					handleSearchChange={handleSearchChange}
-				/><div className="flex top-19 h-max w-full left-0 list flex flex-wrap items-center p-4  rounded-b-lg border-t-[1px] border-slate-200 bg-color-primary-medium border-bg-color-primary-medium" onClick={handleClickDropDown}>
-
-					{filteredIds.length ? filteredIds.map(id => (
-						<CheckboxSelect
-							key={id}
-							id={id}
-							checked={activeLocations.includes(id)}
-							onChange={(event) => handleParamsChange(event, name,activeLocations)}
-						/>
-					)) : "No found"}
+						{filteredIds.length ? filteredIds.map(id => (
+							<CheckboxSelect
+								key={id}
+								id={id}
+								checked={activeLocations.includes(id)}
+								onChange={(event) => handleParamsChange(event, name, activeLocations)}
+							/>
+						)) : "No found"}
 
 
 
