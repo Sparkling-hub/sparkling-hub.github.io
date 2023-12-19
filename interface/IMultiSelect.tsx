313a90fd765060e666bbc205db5e2a9ae6fa842f
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-19 18:16:07 +0200

    WEB-24 <fix smell_17>

--- interface/IMultiSelect.tsx
+++ interface/IMultiSelect.tsx
@@ -5,17 +5,11 @@ interface IMultiSelect {
     name: string;
     uniqueIds: string[]
     activeLocations: string[]
-    placeholder: string    
-<<<<<<< HEAD
-    handleLocationChange: (event: ChangeEvent<HTMLInputElement>, name:string,active:string[]) => void
-    deleteItem: (e: React.MouseEvent<HTMLSpanElement>, name: string, active: string[])=> void
- }
-=======
+    placeholder: string  
 
     handleParamsChange: (event: ChangeEvent<HTMLInputElement>, name:string,active:string[]) => void
     deleteItem: (e: React.MouseEvent<HTMLSpanElement>, name: string, active: string[])=> void    
   }
->>>>>>> fd9ca2c7c5970948b709f2f195055ae7692b3064
   
 export default IMultiSelect
 
