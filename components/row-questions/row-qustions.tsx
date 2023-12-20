9d2f65a4452223632fe8de1048f481e327106114
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-20 18:25:02 +0200

    WEB-24 <Did a code review. >

--- components/row-questions/row-qustions.tsx
+++ components/row-questions/row-qustions.tsx
@@ -1,24 +1,29 @@
+
+
 import React from 'react';
 import type IQustians from '../../interface/IQustion';
-
 import DisabledSelect from '../ui/select-questions/select';
-import questions from '../../data/data-questions';  
+import questions from '../../data/data-questions';
+
 
 const Questions: React.FC = () => {
-  return (
-    <div className="p-4">
-      
-      {Object.values(questions.question).map((value:IQustians, index) => (
 
-   
-<div className="relative m-5 " key={index}>
-      <DisabledSelect data={value}/>
+	return (
 
- 
-      </div>
-    ))};
-       </div>
-  )
+		<div className="p-4">
+
+			{Object.values(questions.question).map((value: IQustians, index) => (
+
+				<div className="relative m-5 " key={index}>
+
+					<DisabledSelect data={value} />
+
+				</div>
+
+			))};
+
+		</div>
+	)
 
 };
 
--- components/startup-step-by-step/StartupStepByStep.tsx
+++ components/startup-step-by-step/StartupStepByStep.tsx
@@ -1,44 +1,60 @@
+
+
 import React, { useEffect, useState } from 'react';
 import dataStartupSteps from '@/data/data-sections/data-section-startup/data-startup-step-by-step';
 import StartupStepItem from '../startup-step-item';
 import Button from '../button';
 
 const StartupStepByStep: React.FC = () => {
-  const [highlightedIndices, setHighlightedIndices] = useState<number[]>([]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const elements = document.querySelectorAll('.s-b-s-title');
+	const [highlightedIndices, setHighlightedIndices] = useState<number[]>([]);
 
-      elements.forEach((element, index) => {
-        const rect = element.getBoundingClientRect();
-        const isVisible = rect.top < window.innerHeight / 2 && rect.bottom >= 0;
+	useEffect(() => {
 
-        if (isVisible && !highlightedIndices.includes(index+1)) {
-          setHighlightedIndices((prevIndices) => [...prevIndices, index+1]);
-        }
-      });
-    };
+		const handleScroll = () => {
 
-    window.addEventListener('scroll', handleScroll);
+			const elements = document.querySelectorAll('.s-b-s-title');
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [highlightedIndices]);
+			elements.forEach((element, index) => {
 
-  return (
-    <div className='flex flex-wrap justify-center items-center'>
-      <ol>
-        {dataStartupSteps.map((step) => (
-          <StartupStepItem key={Number(step.index)} {...step} highlighted={highlightedIndices.includes(Number(step.index))} />
-        ))}
-      </ol>
-      <div className='m-20'>
-        <Button href="/contact" text="Embark in your MVP adventure!" />
-      </div>
-    </div>
-  );
+				const rect = element.getBoundingClientRect();
+				const isVisible = rect.top < window.innerHeight / 2 && rect.bottom >= 0;
+
+				if (isVisible && !highlightedIndices.includes(index + 1)) {
+					setHighlightedIndices((prevIndices) => [...prevIndices, index + 1]);
+				}
+
+			});
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+
+			window.removeEventListener('scroll', handleScroll);
+
+		};
+
+	}, [highlightedIndices]);
+
+	return (
+
+		<div className='flex flex-wrap justify-center items-center'>
+
+			<ol>
+
+				{dataStartupSteps.map((step) => (
+					<StartupStepItem key={Number(step.index)} {...step} highlighted={highlightedIndices.includes(Number(step.index))} />
+				))}
+
+			</ol>
+
+			<div className='m-20'>
+				<Button href="/contact" text="Embark in your MVP adventure!" />
+			</div>
+
+		</div>
+	);
 };
 
 export default StartupStepByStep;
