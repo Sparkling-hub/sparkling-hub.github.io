c58d2b4f28cac3b9c56b371c14486a37793ea87b
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2023-12-22 11:57:00 +0100

    Merge branch 'dev' into WEB-21-FAQ-FIX

--- components/ui/select-questions/select.tsx
+++ components/ui/select-questions/select.tsx
@@ -12,9 +12,9 @@ const DisabledSelect: FC<{ data: IQustion }> = ({ data }) => {
 
   return (
     <>
-      <div
+      <button
         className={`w-full p-3 border rounded cursor-pointer text-lg border-none ${
-          isDropdownOpen ? 'bg-emerald-900 text-teal-500 rounded-t-2xl' : 'rounded-2xl bg-slate-100'
+          isDropdownOpen ? 'bg-emerald-900 text-white rounded-t-2xl' : 'rounded-2xl bg-slate-100'
         }`}
         role='button'
         onClick={handleDropdownToggle}
@@ -22,7 +22,7 @@ const DisabledSelect: FC<{ data: IQustion }> = ({ data }) => {
         onKeyDown={handleDropdownToggle}
       >
         <span className="block relative flex justify-between ">{data.question}<ButtonCircle isDropdownOpen={isDropdownOpen} /> </span>
-      </div>
+      </button>
       {isDropdownOpen && (
         <div className="top-full left-0 bg-white border border-gray-300 rounded-b-2xl p-4 w-full">
           <p>{data.answer}</p>
