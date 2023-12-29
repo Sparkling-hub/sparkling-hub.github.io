94ac69ad679ee1379a8200789cebb355cf80ae09
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2023-12-29 11:57:31 +0200

    WEB-46 <add page>

---
+++ data/data-sections/data-section-consulting/index.tsx
@@ -0,0 +1,10 @@
+
+import type  ISConsultingData  from "@/interface/IConsulting";
+
+
+const sections: ISConsultingData[] = [
+  
+];
+
+export default sections
+
---
+++ interface/IConsulting.tsx
@@ -0,0 +1,14 @@
+
+
+interface IConsulting {
+
+    header?: JSX.Element;  
+    content?: JSX.Element;  
+    button?: JSX.Element;  
+    padding_bottom?: string;
+    curvedClass?: string;
+    index?: number;
+	
+}
+
+export default IConsulting
