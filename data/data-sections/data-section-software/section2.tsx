bd96078b789c649e6b62495964ffbebf0987886e
JlallyX <nastyj2003@gmail.com>
2024-01-02 15:47:09 +0200

    add section two

--- data/data-sections/data-section-software/section2.tsx
+++ data/data-sections/data-section-software/section2.tsx
@@ -1,32 +1,23 @@
 
-import StartupsItemList from "@/components/startup-item-list"
+import StartupsTechnologiesIconList from "@/components/startup-technologies-icon-list"
 
 
 
 const header =
-	<h2 className="flex-col items-center  text-6xl mt-24">
-		<div className="flex justify-center">
-			<span className="color-primary-light mx-4">Your</span>
-			<span className="text-black font-medium">Dream Team</span>
-			<br />
-		</div>
-		<div className="flex justify-center my-8">
-			<span className=" mx-4 text-3xl ">Experts dedicated to their craft and fully committed to your project.</span>
-			<br />
-		</div>
-	</h2>
+  <h2 className="flex-col items-center justify-center p-4">
+ 
+  </h2>
 
-const content = <StartupsItemList />
+const content = <p className="text-white text-center text-lg">Want to develop your project? Or perhaps enhance your development efforts?<br/><span className="text-teal-300 font-semibold">With Sparkling you make a collaborative partnership, not simple outsourcing.</span></p>
 
 
 const element = {
-	content: content,
-	header: header,
-	// curvedClass: 'before:content-curved_white_with_line before:w-full before:absolute before:-mt-curved-startup ',
-	curvedClass: 'before:content-curved_white_with_line before:w-full before:absolute',
-	background: 'bg-color-primary-light',
-	index: 2,
-	padding_bottom: 'pb-40',
-	margin_bottom: 'mb-64'
+  content: content,
+  header: header,
+  index: 3,
+  curvedClass: 'before:content-curved_dark_without_line before:w-full before:absolute',
+  background: 'bg-color-primary-dark',
+  margin_bottom: 'mb-12'
+
 }
 export default element
