ae1afe9e0ad2d22e0c685a618bcd0b427e423df9
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2024-01-04 00:53:36 +0200

    WEB-32 <fix smell_4>

--- data/data-maps/index.tsx
+++ data/data-maps/index.tsx
@@ -1,16 +1,10 @@
 
 
-import mapsWorld from "./maps-WORLD";
-import mapsEU from "./maps-EU";
-import mapsAF from "./maps-AF";
 import mapsAll from "./maps-all";
 import type  Imaps  from "../../interface/IMaps";
 
 const maps: Imaps[] = [
-
-  mapsWorld,
-  mapsEU,
-  mapsAF,
+ 
   mapsAll,
  
 ];
