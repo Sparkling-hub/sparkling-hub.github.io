17b490575bf0b64e23a90cbf03e0c9e1f6c09db8
JlallyX <nastyj2003@gmail.com>
2024-01-04 17:56:18 +0200

    fix problem

--- data/data-sections/data-sections-home/section3.tsx
+++ data/data-sections/data-sections-home/section3.tsx
@@ -4,30 +4,31 @@ import StartupsTechnologiesIconList from "@/components/startup-technologies-icon
 
 
 const header =
-  <h2 className="flex-col items-center justify-center p-4">
-    <div className="flex justify-center text-5xl mb-4">
-        <span className="color-primary-light font-medium mx-4">Technologies</span> 
-        <span className="text-white font-light">We Work With</span>
-    </div>
-    <div className="flex justify-center text-xl">
-        <span className="text-white font-normal ">Front-end, Back-end and DevOps;</span> 
-        <span className="text-white font-light mx-2">We use the</span>  
-        <span className="text-white font-normal ">right tools for the right job!</span>
-    </div>
-    
-  </h2>
+  <div className="m-auto my-16 text-center">
+    <h2 className=" text-5xl  font-light text-white mb-5">
+      <span className="text-teal-400 font-medium">Technologies </span>
+      We Work With
+    </h2>
+    <p className="text-white font-light text-xl">
+      <span className="font-medium">Front-end, Back-end and DevOps. </span>
+      We use the
+      <span className="font-medium"> right tools </span>
+      for
+      <span className="font-medium"> the right job! </span>
+    </p>
+  </div>
 
 const content = <StartupsTechnologiesIconList />
 
 
 const element = {
   content: content,
-  header: header,  
+  header: header,
   id: 'technologies',
-  index: 3,  
+  index: 3,
   curvedClass: 'before:content-curved_dark_without_line before:w-full before:absolute',
-	background: 'bg-color-primary-dark', 
-	margin_bottom: 'mb-12',
+  background: 'bg-color-primary-dark',
+  margin_bottom: 'mb-12',
   padding_bottom: "pb-12"
 
 }
