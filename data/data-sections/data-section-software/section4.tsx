f0439ed9bc233a861c85eef6d338aedc4d64a9c5
JlallyX <nastyj2003@gmail.com>
2024-01-04 08:15:04 +0200

    Update section4.tsx

--- data/data-sections/data-section-software/section4.tsx
+++ data/data-sections/data-section-software/section4.tsx
@@ -4,10 +4,7 @@ import Manifest from "@/components/manifest"
 const header =
     <div className="text-center mt-12 mb-12 leading-7 w-2/3 mx-auto">
         <h2 className="text-5xl m-4">
-            <strong className="text-teal-500 font-bold">
-                Agile
-            </strong>
-            with a kick
+            <strong className="text-teal-500 font-bold">Agile</strong> with a kick
         </h2>
         <p className="text-center">
             As passionate believers in the agile manifesto we champion iterative development, embracing retrospectives,
