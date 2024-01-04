ebe0a5f31a69bd9fd248dd1d8e505718133d1a74
JlallyX <nastyj2003@gmail.com>
2024-01-04 17:20:57 +0200

    redid in svg

--- components/startup-icon/StartupIcon.tsx
+++ components/startup-icon/StartupIcon.tsx
@@ -12,7 +12,7 @@ const StartupIcon: React.FC<IStartupTechnologiesItem> = ({ imageSrc, text }) => 
 
 			<div className=' flex startup-icon justify-center items-center bg-white w-48 h-48 rounded-2xl m-4 '>
 
-				<img className='w-24 h-24 inline-block mx-4 ' src={imageSrc} alt="" />
+				<img className='w-24 h-24 inline-block mx-4' src={imageSrc} alt="" />
 				<div className='text-center  p-4 color-primary-dark font-medium'> {text}</div>
 				
 			</div>
