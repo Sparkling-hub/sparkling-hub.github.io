5b9d22fdb1e71dd01a690acb86529acc3cf408a9
JlallyX <nastyj2003@gmail.com>
2024-01-04 20:09:29 +0200

    Update section3.tsx

--- data/data-sections/data-section-startup/section3.tsx
+++ data/data-sections/data-section-startup/section3.tsx
@@ -5,14 +5,15 @@ import StartupsTechnologiesIconList from "@/components/startup-technologies-icon
 const header = (
   <div className="m-auto my-20 text-center">
     <h2 className="text-5xl font-light text-white mb-5">
-      <span className="text-teal-400 font-medium">Technologies</span>{/*
-  */}{' '}We Work With
+      <span className="text-teal-400 font-medium">Technologies{' '}{/*
+  */}</span>We Work With
     </h2>
     <p className="text-white font-light text-xl">
-      <span className="font-medium">Front-end, Back-end and DevOps.</span>{/*
-  */}{' '}We use the{' '}
-      <span className="font-medium">right tools</span>
-        {' '}for{/*
+      <span className="font-medium">Front-end, Back-end and DevOps.{' '}</span>{/*
+  */}We use the{' '}{/*
+  */}<span className="font-medium">right tools{' '}</span>
+      {/*
+  */}for{/*
   */}<span className="font-medium">{' '}the right job!</span>
     </p>
   </div>
