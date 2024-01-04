3d3ce200319573af39276c209fb5a0f2f378214d
JlallyX <nastyj2003@gmail.com>
2024-01-04 11:48:21 +0200

    redid style

--- components/service/Service.tsx
+++ components/service/Service.tsx
@@ -7,7 +7,7 @@ import IService from '../../interface/service'
 const Service: React.FC<IService> = ({ content, header, image, button }) => {
 
 	return (
-		<div className='flex-col max-w-sm pb-40'>
+		<div className='flex-col w-1/3 pb-40 h-full ml-[15px]'>
 			<div className='service_icon'>{image} </div>
 			{header}
 			{content}
