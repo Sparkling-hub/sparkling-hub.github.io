c401d1955a3283de9263d86dfbc21c3ab4ef59c6
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2024-01-04 12:39:49 +0200

    WEB-46 <add section1>

--- data/data-sections/data-section-consulting/index.tsx
+++ data/data-sections/data-section-consulting/index.tsx
@@ -1,9 +1,10 @@
 
-import type  ISConsultingData  from "@/interface/IConsulting";
 
+import type  ISConsultingData  from "@/interface/IConsulting";
+import Section1 from "./section1";
 
 const sections: ISConsultingData[] = [
-  
+    Section1,
 ];
 
 export default sections
