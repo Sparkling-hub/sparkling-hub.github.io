63160d98caf50c0407ef7e07b09193dec9155f7e
JlallyX <nastyj2003@gmail.com>
2024-01-04 20:19:59 +0200

    Update section3.tsx

--- data/data-sections/data-section-startup/section3.tsx
+++ data/data-sections/data-section-startup/section3.tsx
@@ -5,10 +5,8 @@ import StartupsTechnologiesIconList from "@/components/startup-technologies-icon
 const header = (
   <div className="m-auto my-20 text-center">
     <h2 className="text-5xl font-light text-white mb-5">{/*
-  */}<span className="text-teal-400 font-medium">Technologies{' '}</span>{/*
-  */}We Work With{/*
-  */}
-    </h2>
+  */}<span className="text-teal-400 font-medium">Technologies</span>{' '}We Work With{/*
+  */}</h2>
     <p className="text-white font-light text-xl">{/*
   */}<span className="font-medium">Front-end, Back-end and DevOps.</span>{/*
   */}We use the{' '}{/*
