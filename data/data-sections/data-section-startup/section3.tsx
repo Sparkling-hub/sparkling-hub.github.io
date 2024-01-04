3e4592b4d30531d10249c2fa077dcdeaff84c455
JlallyX <nastyj2003@gmail.com>
2024-01-04 18:54:10 +0200

    Update section3.tsx

--- data/data-sections/data-section-startup/section3.tsx
+++ data/data-sections/data-section-startup/section3.tsx
@@ -6,18 +6,17 @@ import StartupsTechnologiesIconList from "@/components/startup-technologies-icon
 const header =
   <div className="m-auto my-16 text-center">
     <h2 className=" text-5xl  font-light text-white mb-5">
-      <span className="text-teal-400 font-medium">Technologies
-      </span>
-      We Work With
+      <span className="text-teal-400 font-medium">Technologies</span>
+      {' '}We Work With
     </h2>
     <p className="text-white font-light text-xl">
       <span className="font-medium">Front-end, Back-end and DevOps.
       </span>
       We use the
-      <span className="font-medium"> right tools
+      <span className="font-medium">{' '}right tools
       </span>
       for
-      <span className="font-medium"> the right job!
+      <span className="font-medium"> {' '}the right job!
       </span>
     </p>
   </div>
