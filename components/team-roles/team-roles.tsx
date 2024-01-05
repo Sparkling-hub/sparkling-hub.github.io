3638c40280586a389a15dba6d97dcacd237c81d2
JlallyX <nastyj2003@gmail.com>
2024-01-05 15:01:48 +0200

    redid style

--- components/team-roles/team-roles.tsx
+++ components/team-roles/team-roles.tsx
@@ -24,15 +24,16 @@ const Benefit: React.FC = () => {
                             {'\u00A0'}We Cover
                         </strong>:
                     </h3>
-                    <ul>
+                    <ul className='flex flex-row flex-wrap p-5'>
+                    
                         {rolesData.map((role) => (
-                            <li className="flex flex-row m-3" key={role}><img className='w-6 h-6 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />{role}</li>
+                            <li className="flex flex-row my-2 mx-[5%] w-[40%]" key={role}><img className='w-6 h-6 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />{role}</li>
                         ))}
                     </ul>
                 </div>
 
 
-                <div className='border-gray-50 h-1/4 bg-gray-100 border-[1px] rounded-3xl mx-5 mb-2  mt-8 h-[35%] flex items-center justify-center p-2' >
+                <div className='border-gray-50 h-1/3 bg-gray-100 border-[1px] rounded-3xl mx-5 mb-2  mt-12 h-[35%] flex items-center justify-center p-2' >
                     <Button href='/contact#rec_form' text={"Build your team"} />
                 </div>
             </div>
