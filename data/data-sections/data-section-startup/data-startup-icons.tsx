25b61ffb0d406bbcf76688b1dc1f0b9f7a8a0b32
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-01-06 19:33:55 +0200

    WEB-61-Home-Page-section-3-Smaller-buttons-enlargement-and-color (#58)
    * Update StartupTechnologiesIconList.tsx
    
    * redid style svg and fix image
    
    ---------
    
    Co-authored-by: Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>

--- data/data-sections/data-section-startup/data-startup-icons.tsx
+++ data/data-sections/data-section-startup/data-startup-icons.tsx
@@ -3,53 +3,53 @@ import IStartupTechnologiesItem from '@/interface/IStartupTechnologiesItem';
 
 const startupIcon: IStartupTechnologiesItem[] = [
 	{
-		imageSrc: '/img/startup/icon-technologies/Javascript.png',
+		imageSrc: '/img/startup/icon-technologies/Javascript.svg',
 		text: "Javascript",
 		index: 1,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/Python.png',
+		imageSrc: '/img/startup/icon-technologies/Python.svg',
 		text: "Python",
 		index: 2,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/Java.png',
+		imageSrc: '/img/startup/icon-technologies/Java.svg',
 		text: "Java",
 		index: 3,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/Node_Js.png',
+		imageSrc: '/img/startup/icon-technologies/Node_Js.svg',
 		text: "Node js",
 		index: 4,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/React_Js.png',
+		imageSrc: '/img/startup/icon-technologies/React_Js.svg',
 		text: "React Js",
 		index: 5,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/Typescript.png',
+		imageSrc: '/img/startup/icon-technologies/Typescript.svg',
 		text: "Typescript",
 		index: 6,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/Angular.png',
+		imageSrc: '/img/startup/icon-technologies/Angular.svg',
 		text: "Angular",
 		index: 7,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/AWS.png',
+		imageSrc: '/img/startup/icon-technologies/AWS.svg',
 		text: "AWS",
 		index: 8,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/C++.png',
+		imageSrc: '/img/startup/icon-technologies/C++.svg',
 		text: "C++",
 		index: 9,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/PHP.png',
-		text: "PHP#",
+		imageSrc: '/img/startup/icon-technologies/PHP.svg',
+		text: "PHP",
 		index: 10,
 	},
 
