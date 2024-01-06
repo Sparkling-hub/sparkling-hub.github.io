906538c1f2339bed0c6c586b4eb6c2b80c807211
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-01-06 13:29:58 +0200

    redid style (#59)
    * redid style
    
    * WEB-62 <fix margin-y sections>
    
    ---------
    
    Co-authored-by: Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>

--- components/sections/sections.tsx
+++ components/sections/sections.tsx
@@ -8,7 +8,7 @@ const Section: React.FC<SectionType> = ({ header, content, image, button, backgr
 
 	return (
 
-		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc pb-20  xl:px-40 custom-3xl`} id={id}>
+		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc py-40  xl:px-40 custom-3xl`} id={id}>
 			<div className='max-w-screen-xl items-start'>
 				<div className='content-section flex   items-start justify-center flex-col justify-center'>
 
