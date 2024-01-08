c4c08bedd022d2a19d212e351cb4f5fa4a1b0d9e
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2024-01-08 17:57:29 +0200

    Web 68 footer bug (#65)
    * redid style
    
    * WEB-68 <fix footer margin and curved elements>
    
    ---------
    
    Co-authored-by: JlallyX <nastyj2003@gmail.com>

--- data/data-sections/data-section-faq/section_two.tsx
+++ data/data-sections/data-section-faq/section_two.tsx
@@ -5,7 +5,7 @@ const header =
   <h3>Here are our most common questions:</h3>
 
 
-const content = <><Questions/><h3>Any other burning questions?</h3>
+const content = <><Questions/><h3 className="mb-12">Any other burning questions?</h3>
 </>
 const button= <Button href={"/contact"} text={"Let us know"}/>
 
@@ -13,7 +13,8 @@ const button= <Button href={"/contact"} text={"Let us know"}/>
 const element = {
   content: content,
   header: header,
-  background:'teal-50',
+  curvedClass: 'before:content-curved_white_with_line before:w-full before:absolute',
+  background: 'bg-color-primary-light',
   id: "answers",
   button:button
 }
