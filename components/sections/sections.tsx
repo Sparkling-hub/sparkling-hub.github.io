5b710b4d82fe3fa2213a5629cd79fde13ba0ac59
Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>
2024-01-09 10:26:19 +0200

    Update sections.tsx

--- components/sections/sections.tsx
+++ components/sections/sections.tsx
@@ -7,7 +7,7 @@ import type SectionType from '../../interface/section';
 const Section: React.FC<SectionType> = ({ header, content, image, button, background, id, curvedClass }) => {
 
 	return (
-		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc pb-20  xl:px-40 custom-3xl`} id={id}>
+		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc py-20  xl:px-40 custom-3xl`} id={id}>
 			<div className='max-w-screen-xl w-full items-start mb-9 '>
 				<div className='content-section flex items-start justify-center flex-col justify-center z-10'>
 
