c3debc86f410a76c20cdb0040449ef6e356a70da
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-01-09 15:52:20 +0200

    WEB-68-Footer-bug in dev (#68)
    * fix
    
    * Update Button.tsx
    
    * Update sections.tsx
    
    ---------
    
    Co-authored-by: Maks_sol_2016 <33526140+SologubovMaksim2016@users.noreply.github.com>

--- components/sections/sections.tsx
+++ components/sections/sections.tsx
@@ -7,9 +7,9 @@ import type SectionType from '../../interface/section';
 const Section: React.FC<SectionType> = ({ header, content, image, button, background, id, curvedClass }) => {
 
 	return (
-		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc pb-20  xl:px-40 custom-3xl`} id={id}>
+		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc py-20  xl:px-40 custom-3xl`} id={id}>
 			<div className='max-w-screen-xl w-full items-start mb-9 '>
-				<div className='content-section flex items-start justify-center flex-col justify-center'>
+				<div className='content-section flex items-start justify-center flex-col justify-center z-10'>
 
 					<div className='flex items-center whitespace-normal w-full'>
 						<div className='flex justify-center flex-col w-full my-12'>{header}
