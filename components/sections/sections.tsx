a34981dc6bc4741ed29240b8d2797c7d5e432407
JlallyX <nastyj2003@gmail.com>
2024-01-09 02:03:12 +0200

    fix

--- components/sections/sections.tsx
+++ components/sections/sections.tsx
@@ -9,7 +9,7 @@ const Section: React.FC<SectionType> = ({ header, content, image, button, backgr
 	return (
 		<section className={`flex items-center justify-center relative ${background} ${curvedClass} before:-top-calc pb-20  xl:px-40 custom-3xl`} id={id}>
 			<div className='max-w-screen-xl w-full items-start mb-9 '>
-				<div className='content-section flex items-start justify-center flex-col justify-center'>
+				<div className='content-section flex items-start justify-center flex-col justify-center z-10'>
 
 					<div className='flex items-center whitespace-normal w-full'>
 						<div className='flex justify-center flex-col w-full my-12'>{header}
