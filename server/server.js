e782c7518460036e4106145b0eb439134c9867af
JlallyX <nastyj2003@gmail.com>
2024-01-17 09:56:33 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 let corsOptions = {
-    origin: '*' // Sensitive
+    
 };
 const multer = require("multer");
 const { body, validationResult } = require("express-validator");
