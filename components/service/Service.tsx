e14aeb66ebc86fce087ec68d89ffdbc42c2c0893
JlallyX <nastyj2003@gmail.com>
2024-01-22 18:48:24 +0200

    fix problem with screen

--- components/service/Service.tsx
+++ components/service/Service.tsx
@@ -7,7 +7,7 @@ import IService from '../../interface/service'
 const Service: React.FC<IService> = ({ content, header, image, button }) => {
 
 	return (
-		<div className='flex-col pb-[40px] w-[330px] lg:w-1/3 lg:p-[20px] h-full text-center lg:h-[700px]'>
+		<div className='flex-col pb-[40px] w-full lg:w-1/3 lg:p-[20px] h-full text-center lg:h-[700px]'>
 			<div className='service_icon flex justify-center h-96 lg:h-[200px] w-full'>{image} </div>
 			<div className='mb-4 lg:mb-8  flex items-first justify-center text-4xl text-medium lg:h-[80px]'>{header}</div>
 			<div className='2xl:h-[300px] xl:h-[350px] text-xl lg:h-[370px]'>{content}</div>
