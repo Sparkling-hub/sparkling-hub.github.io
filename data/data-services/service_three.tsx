9bb265f5106550991cddd321ddddce96678337b9
JlallyX <nastyj2003@gmail.com>
2024-01-26 00:22:38 +0200

    finish style

--- data/data-services/service_three.tsx
+++ data/data-services/service_three.tsx
@@ -7,11 +7,11 @@ const header =
 
 
 const content =		<div className="lg:mb-12">
-<p className='2xl:h-[160px] lg:h-[170px] text-xl'>
+<p className='2xl:h-[190px] xl:h-[180px] lg:h-[200px] text-xl'>
 			Revamp your product strategy with personalized guidance addressing complex challenges with tailored solutions.
 			Our experts are here to steer you back on the right track.
 		</p>
-		<ul className='text-left text-xl font-semibold  lg:text-xl w-fit m-auto flex flex-col whitespace-nowrap 2xl:p-0 p-4'>
+		<ul className='text-left text-xl font-semibold  lg:text-xl w-fit m-auto flex flex-col  whitespace-nowrap lg:whitespace-normal 2xl:p-0 p-4'>
 			<li>
 				<img className='w-4 h-4 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />Research & Development (R&D)
 			</li>
