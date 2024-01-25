168d45e9268e2281eac1ab5e5a82ab3bf08ca9e6
JlallyX <nastyj2003@gmail.com>
2024-01-25 16:25:35 +0200

    add mobile design

--- components/startup-technologies-icon-list/StartupTechnologiesIconList.tsx
+++ components/startup-technologies-icon-list/StartupTechnologiesIconList.tsx
@@ -8,7 +8,7 @@ const StartupTechnologiesIconList: React.FC = () => {
   const duplicatedIcons = [...dataStartups, ...dataStartups];
 
   return (
-    <div className='flex flex-col justify-center items-center  overflow-hidden relative right-[110px]'>
+    <div className='flex flex-col justify-center items-center  overflow-hidden block 2xl:relative 2xl:w-[1500px] right-[110px]'>
       <div className='container marquee-track'>
         <div className='grid grid-flow-col grid-cols-1 grid-rows-1 gap-4 w-fit iconContainer marquee-track'>
 
