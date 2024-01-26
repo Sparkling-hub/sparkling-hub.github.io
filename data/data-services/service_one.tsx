4f1d9421aa41504a1a06f0863e788a1bf6b3ce3c
JlallyX <nastyj2003@gmail.com>
2024-01-26 14:26:08 +0200

    Added flex in li

--- data/data-services/service_one.tsx
+++ data/data-services/service_one.tsx
@@ -9,14 +9,14 @@ const header =
 const content =
 <div className="lg:mb-12">
 <p className='2xl:h-[190px] xl:h-[180px] lg:h-[200px] text-xl'>Enhance your development efforts by extending your existing team or build your new team from scratch. Customise it to your needs to create a perfect fit!</p>
-<ul className='text-left text-xl font-semibold lg:text-xl w-fit m-auto flex flex-col  lg:whitespace-normal  2xl:p-0 py-4'>
-			<li>
+<ul className='text-left text-xl font-semibold lg:text-xl w-fit m-auto flex flex-col lg:whitespace-normal  2xl:p-0 py-4'>
+			<li className='flex'>
 				<img className='w-4 h-4 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />Full stack develoment
-			</li>
-			<li>
+			</li  >
+			<li className='flex'>
 				<img className='w-4 h-4 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />Technical support
 			</li>
-			<li>
+			<li className='flex'>
 				<img className='w-4 h-4 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />Agile Collaboration
 			</li>
 		</ul>
