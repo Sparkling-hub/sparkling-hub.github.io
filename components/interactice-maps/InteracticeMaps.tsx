d0045768a0421458a637d83a62c0786852e60c6b
JlallyX <nastyj2003@gmail.com>
2024-01-26 10:44:36 +0200

    fix problem

--- components/interactice-maps/InteracticeMaps.tsx
+++ components/interactice-maps/InteracticeMaps.tsx
@@ -82,8 +82,7 @@ const InteracticeMaps: React.FC = () => {
 	const setupPointEventListeners = (points: NodeListOf<Element>) => {
 		points.forEach((element) => {
 			element.addEventListener('click', handlePointsClick);
-			element.addEventListener('mouseover', handleHoverOver);
-			element.addEventListener('mouseout', handleHoverOut);
+
 		});
 	};
 
