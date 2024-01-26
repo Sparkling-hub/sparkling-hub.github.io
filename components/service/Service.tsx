8b6bc1654a893394c6633097beb9d2822c1a5b05
JlallyX <nastyj2003@gmail.com>
2024-01-26 15:49:45 +0200

    Code indentation

--- components/service/Service.tsx
+++ components/service/Service.tsx
@@ -10,7 +10,7 @@ const Service: React.FC<IService> = ({ content, header, image, button, index }) 
 		<div className={`flex-col pb-[50px] w-full lg:w-1/3 lg:px-[20px] h-full text-center 2xl:h-[700px] xl:h-[750px] lg:h-[800px] bg-white m-2 rounded-3xl ${(index)%2==0?'lg:mt-[100px]':""}`}>
 			<div className='service_icon flex justify-center w-96 lg:h-[200px] w-full'>{image} </div>
 			<div className='mb-4 lg:mb-8  flex items-first justify-center text-4xl text-medium lg:h-[80px]'>{header}</div>
-			<div className='2xl:h-[300px] xl:h-[350px] text-xl px-[30px] lg:px-0   lg:h-[400px]'>{content}</div>
+			<div className='2xl:h-[300px] xl:h-[350px] text-xl px-[30px] lg:px-0 lg:h-[400px]'>{content}</div>
 			<div className='flex justify-center my-2'>{button}</div>
 		</div>
 
--- data/data-services/service_one.tsx
+++ data/data-services/service_one.tsx
@@ -7,10 +7,10 @@ const header =
 
 
 const content =
-<div className="lg:mb-12">
-<p className='2xl:h-[190px] xl:h-[180px] lg:h-[200px] text-xl'>Enhance your development efforts by extending your existing team or build your new team from scratch. Customise it to your needs to create a perfect fit!</p>
-<ul className='text-left text-xl font-semibold lg:text-xl w-fit m-auto flex flex-col lg:whitespace-normal  2xl:p-0 py-4'>
-				<li className='flex items-baseline'>
+	<div className="lg:mb-12">
+		<p className='2xl:h-[190px] xl:h-[180px] lg:h-[200px] text-xl'>Enhance your development efforts by extending your existing team or build your new team from scratch. Customise it to your needs to create a perfect fit!</p>
+		<ul className='text-left text-xl font-semibold lg:text-xl w-fit m-auto flex flex-col lg:whitespace-normal  2xl:p-0 py-4'>
+			<li className='flex items-baseline'>
 				<img className='w-4 h-4 inline-block mx-2' src='/img/right-arrow-circle.svg' alt="" />Full stack develoment
 			</li  >
 			<li className='flex items-baseline'>
