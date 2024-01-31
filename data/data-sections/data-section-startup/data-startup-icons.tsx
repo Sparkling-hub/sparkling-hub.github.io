25e21b5cb0765c3bab0f76584b5358888f82cce4
JlallyX <nastyj2003@gmail.com>
2024-01-31 12:20:16 +0200

    redid data

--- data/data-sections/data-section-startup/data-startup-icons.tsx
+++ data/data-sections/data-section-startup/data-startup-icons.tsx
@@ -10,33 +10,37 @@ const startupIcon: IStartupTechnologiesItem[] = [
 	{
 		imageSrc: '/img/startup/icon-technologies/Typescript.svg',
 		text: "Typescript",
-		index: 6,
+		index: 2,
 	},
 	{
 		imageSrc: '/img/startup/icon-technologies/React_Js.svg',
 		text: "React Js",
+		index: 3,
+	},
+		
+	{
+		imageSrc: '/img/startup/icon-technologies/next.svg',
+		text: "Next js",
+		index: 4,
+	},
+	{
+		imageSrc: '/img/startup/icon-technologies/Angular.svg',
+		text: "Angular",
 		index: 5,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/Java.svg',
-		text: "Java",
-		index: 3,
+		imageSrc: '/img/startup/icon-technologies/vue.svg',
+		text: "vue",
+		index: 6,
 	},
 	{
 		imageSrc: '/img/startup/icon-technologies/Node_Js.svg',
 		text: "Node js",
-		index: 4,
-	},
-	
-
-	{
-		imageSrc: '/img/startup/icon-technologies/Angular.svg',
-		text: "Angular",
 		index: 7,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/AWS.svg',
-		text: "AWS",
+		imageSrc: '/img/startup/icon-technologies/C-.svg',
+		text: "C#",
 		index: 8,
 	},
 	{
@@ -45,61 +49,72 @@ const startupIcon: IStartupTechnologiesItem[] = [
 		index: 9,
 	},
 	{
+		imageSrc: '/img/startup/icon-technologies/dot-net-core.svg',
+		text: ".NET",
+		index: 10,
+	},
+	{
 		imageSrc: '/img/startup/icon-technologies/PHP.svg',
 		text: "PHP",
 		index: 11,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/docker.svg',
-		text: "Docker",
+		imageSrc: '/img/startup/icon-technologies/Python.svg',
+		text: "Python",
 		index: 12,
 	},
 	{
-		imageSrc: '/img/startup/icon-technologies/C-.svg',
-		text: "C#",
+		imageSrc: '/img/startup/icon-technologies/django.svg',
+		text: "Django",
 		index: 13,
-	},
-	{
-		imageSrc: '/img/startup/icon-technologies/vue.svg',
-		text: "vue",
-		index: 14,
-	},
-	{
-		imageSrc: '/img/startup/icon-technologies/selenium.svg',
-		text: "Selenium",
-		index: 15,
 	},
 	{
 		imageSrc: '/img/startup/icon-technologies/r-lang.svg',
 		text: "R",
+		index: 14,
+	},
+	{
+		imageSrc: '/img/startup/icon-technologies/Java.svg',
+		text: "Java",
+		index: 15,
+	},
+
+
+
+	{
+		imageSrc: '/img/startup/icon-technologies/AWS.svg',
+		text: "AWS",
 		index: 16,
+	},
+
+
+	{
+		imageSrc: '/img/startup/icon-technologies/docker.svg',
+		text: "Docker",
+		index: 17,
 	},
 	{
 		imageSrc: '/img/startup/icon-technologies/kubernets.svg',
 		text: "Kubernets",
-		index: 17,
-	},
-
-	{
-		imageSrc: '/img/startup/icon-technologies/django.svg',
-		text: "Django",
 		index: 18,
+	},
+	
+	{
+		imageSrc: '/img/startup/icon-technologies/selenium.svg',
+		text: "Selenium",
+		index: 19,
 	},
 	{
 		imageSrc: '/img/startup/icon-technologies/firebase.svg',
 		text: "Firebase",
-		index: 19,
-	},
-	{
-		imageSrc: '/img/startup/icon-technologies/dot-net-core.svg',
-		text: ".NET",
 		index: 20,
 	},
-	{
-		imageSrc: '/img/startup/icon-technologies/Python.svg',
-		text: "Python",
-		index: 2,
-	},
+	
+
+	
+	
+
+
 ]
 
 export default startupIcon
