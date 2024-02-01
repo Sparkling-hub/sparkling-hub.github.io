d4e2ac089498465d1918f547e1860fe4438a23d8
JlallyX <nastyj2003@gmail.com>
2024-02-01 16:33:43 +0200

    Update row-aboutus.tsx

--- components/row-aboutus/row-aboutus.tsx
+++ components/row-aboutus/row-aboutus.tsx
@@ -10,7 +10,7 @@ const Benefits: React.FC = () => {
 	return (
 
 		<div className='flex-col m-5 relative '>
-	<img src='/img/about/principles.svg' className='hidden lg:block relative left-[-200px] z-10'></img>
+	<img src='/img/about/principles.svg' className='hidden lg:block relative left-[-200px] z-10' alt='principles'></img>
 			{dataAbout.map((about) => (
 				<About key={Number(about.number)} {...about} />
 			
