a254b98fa6439b352e3e665f7e5ed94db0d3cca1
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-02-05 16:23:36 +0200

    Web 83 submit button show show unavailable if not available (#92)
    Co-authored-by: Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>

--- components/ui/select-component/select.tsx
+++ components/ui/select-component/select.tsx
@@ -1,41 +1,22 @@
 
 
-import { useDispatch, useSelector } from 'react-redux';
-import { selectNavigation } from '@/store/redusers/navigationReducer';
-import { setActiveOption, selectForm } from '@/store/redusers/FormSliceReduser';
+
 import rolesData from '@/data/data-contact/data-contact';
-import { ChangeEvent, useEffect } from 'react';
 
-interface SelectProps {
-	name: string;	
-}
 
-const Select: React.FC<SelectProps> = ({ name }) => {
+const Select: React.FC<any> = ({ name, onChange,value }) => {
 
-	const dispatch = useDispatch();
+
 	
-	const { lastPageSlug } = useSelector(selectNavigation);
-	const { activeOption } = useSelector(selectForm);
 
-	useEffect(() => {
-		
-		dispatch(setActiveOption(lastPageSlug));		
 
-	}, []);
-
-	const handleInputChange = (e: ChangeEvent<HTMLSelectElement>) => {
-
-		const { value } = e.target;		
-		dispatch(setActiveOption(value));			
-	
-	};
 	
 	return (
 		<select
 			name={name}
 			className={`border border-green-500 rounded-3xl p-4 w-full my-5`}
-			value={activeOption || ''}
-			onChange={handleInputChange}
+			value={value || ''}
+			onChange={onChange}
 		>
 			
 			{Object.entries(rolesData).map(([key, label]) => {
