2544c0798122cd860eee5a773df0a053b0fca44e
JlallyX <nastyj2003@gmail.com>
2024-02-05 16:00:01 +0200

    add mobile design

--- components/startup-step-item/StartupStepItem.tsx
+++ components/startup-step-item/StartupStepItem.tsx
@@ -11,9 +11,9 @@ const StartupStepItem: React.FC<IStartupStepItem> = ({ title, subtext, index, hi
 
 
 	return (
-		<li className={`flex-col px-40 flex items-center relative  pb-[5rem]`}>
+		<li className={`flex-col px-0 lg:px-40 flex items-center relative  pb-[5rem]`}>
 			{highlighted?
-			<svg width="477" height="310" viewBox="0 0 477 402" className={`absolute ${index%2? "top-7 right-[5%]":'top-7 left-[5%]'}`} fill="none" xmlns="http://www.w3.org/2000/svg"  style={{ transform: index % 2 ? '' : 'scale(-1, 1)' }}>
+			<svg width="477" height="310" viewBox="0 0 477 402" className={`absolute ${index%2? "top-7 right-[5%]":'top-7 left-[5%]'} hidden lg:block`} fill="none" xmlns="http://www.w3.org/2000/svg"  style={{ transform: index % 2 ? '' : 'scale(-1, 1)' }}>
 				<path
 					d="M0 3H434C456.091 3 474 20.9086 474 43V359C474 381.091 456.091 399 434 399H0"
 					stroke="#1A5858"
@@ -34,11 +34,11 @@ const StartupStepItem: React.FC<IStartupStepItem> = ({ title, subtext, index, hi
 
 			<div className={`s-b-s-title rounded-3xl p-1 flex bg-primary-yellow border-[3px] border-primary-darkTeal z-20`}>
 		
-				<div className={`text text-primary-darkTeal text-center w-[315px] h-[50px]  flex items-center justify-center font-bold text-2xl ${highlighted ? 'highlight' : ''}`}>{title}</div>
+				<div className={`text-primary-darkTeal text-center w-[315px] h-[50px] flex items-center justify-center font-bold text-2xl ${highlighted ? 'highlight' : ''}`}>{title}</div>
 			</div>
 
 			<div className='s-b-s-text text-center'>
-				<div className='mx-5  h-[10rem] pt-5 px-22 text-xl font-light'>{subtext}</div>
+				<div className='mx-5  h-auto lg:h-[10rem] pt-5 px-22 text-xl font-normal'>{subtext}</div>
 			</div>
 		</li>
 	);
--- data/data-sections/data-section-startup/section4.tsx
+++ data/data-sections/data-section-startup/section4.tsx
@@ -4,19 +4,19 @@ import StartupStepByStep from "@/components/startup-step-by-step"
 
 
 const header =
-    <h2 className="flex-col items-center justify-center  p-4">
-        <div className="flex justify-center text-5xl mb-4">
-            <span className="text-black font-light mx-4">Launching an</span>
-            <span className="text-primary-yellow font-medium pb-5">MVP in 5 simple steps</span>
-        </div>
-        <div className="flex justify-center text-xl">
-            <span className="text-center text-black  font-light px-40 pb-20">
-                Do you have a ground-breaking idea? Want to develop and launch your MVP effectively and affordably? We have got you covered!
+    <div className="pb-20  text-5xl text-center">
+            
+            <h2 className="font-light">Launching an{' '}
+            <span className="text-primary-yellow font-medium pb-2">MVP in 5 simple steps</span>
+            </h2>
+            <br/>
+            <p className="text-2xl font-light text-teal-700 leading-6 ">
+                Do you have a ground-breaking idea? Want to develop and launch your MVP effectively and affordably?<br/> We have got you covered!
                 Our streamlined 5-step process is designed just for that:
-            </span>
-        </div>
+            </p>
+     
 
-    </h2>
+    </div>
 
 const content = <StartupStepByStep />
 
