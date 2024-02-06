697826191dfdcc63e8e76137ad0977a04bcc3797
JlallyX <nastyj2003@gmail.com>
2024-02-06 20:29:00 +0200

    Update Benefit.tsx

--- components/benefit/Benefit.tsx
+++ components/benefit/Benefit.tsx
@@ -8,13 +8,13 @@ const Benefit: React.FC<IBenefit> = ({ content, header, reverse, image }) => {
 	return (
 
 		<div className={`flex justify-between flex-col-reverse items-center text center lg:text-left ${!reverse ? 'lg:flex-row-reverse' : 'lg:flex-row'}  m-10 `}>
-			<div className='basis-1/2 flex-col justify-center flex px-12 text-center items-center lg:text-left lg:items-start lg:p-4 '>
+			<div className='basis-1/2 flex-col justify-center flex  text-center items-center lg:text-left lg:items-start px-0 lg:p-4 '>
 				<div className='flex  my-2 '>
 					<div className='text-teal-700 font-medium text-4xl'>
 						{header}
 					</div>
 				</div>
-				<div className='my-4 text-xl mx-[15%] lg:mx-0'>{content}</div>
+				<div className='my-4 text-xl lg:w-full sm:w-2/3 mx-0'>{content}</div>
 			</div>
 
 			<div className='basis-1/2 flex items-center justify-center'>
