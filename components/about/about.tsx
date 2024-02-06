316651034e1d06017fbafdc04375c7f048b7df79
JlallyX <nastyj2003@gmail.com>
2024-02-06 12:15:05 +0200

    Update about.tsx

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -28,14 +28,14 @@ const Benefit: React.FC<IAbout> = ({ content, header, number,margin }) => {
 		data-aos={`fade`}
 		id={uniqueId}>
 			<div className='lg:hidden bg-teal-500 border-teal-600 border-[1px] font-medium text-2xl p-0.2 absolute rounded-full z-10 flex'>
-				<div className='rounded-full justify-center border-black border-[1px] flex items-center bg-teal-700 w-20 bg-teal-800 h-20 text-center font-bold text-white -4  text-5xl'>{number}</div>
+				<div className='rounded-full justify-center border-black border-[1px] flex items-center bg-teal-700 w-20 bg-teal-800 h-20 text-center font-bold text-whit  text-5xl'>{number}</div>
 
 			</div>
-			<div className='lg:w-[50vw] lg:max-w-[50%] w-full flex-col flex justify-center lg:h-52 p-8 lg:pl-12 lg:pr-4 lg:py-2 bg-green-color rounded-4xl pl-20 relative  mx-10 my-5'>
+			<div className='lg:w-[50vw] lg:max-w-[50%] w-full flex-col flex justify-center lg:h-52 p-8 lg:pl-12 lg:pr-4 lg:py-2 bg-green-color rounded-4xl pl-14 relative  mx-10 my-5'>
 
 
 				
-				<div className='min-h-[60px] leading-8 pl-5 flex flex-col'>
+				<div className='min-h-[60px] leading-8 p-0 lg:pl-5 flex flex-col'>
 					<div className='text-2xl text-teal-800 font-bold uppercase'>{header}</div>
 					<div className='text-lg'>{content}</div></div>
 			</div>
