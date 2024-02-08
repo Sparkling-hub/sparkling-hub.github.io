5be763fed7ecd6062c88c98e1cbd7c959d82e2bf
JlallyX <nastyj2003@gmail.com>
2024-02-08 11:55:09 +0200

    Update Consulting.tsx

--- components/consulting/Consulting.tsx
+++ components/consulting/Consulting.tsx
@@ -7,9 +7,9 @@ const Consulting: React.FC<IConsulting> = ({ content, header, reverse, img }) =>
 
 	return (
 
-		<div className={`flex justify-between items-center ${!reverse ? 'flex-row-reverse ml-60' : 'mr-60'}  m-20  bg-color-primary-light-light rounded-4xl`}>
+		<div className={`flex flex-col-reverse ustify-between items-center ${!reverse ? 'lg:flex-row-reverse lg:ml-60' : 'lg:flex-row lg:mr-60'} m-5 lg:m-20  bg-color-primary-light-light rounded-4xl`}>
 
-			<div className={`basis-5/6 flex-col justify-center  p-4 ${!reverse ? 'pl-10' : 'pr-15'}`}>
+			<div className={`lg:basis-5/6 flex-col justify-center  p-4 ${!reverse ? 'pl-10' : 'pr-15'}`}>
 
 				<div className='flex my-2 ml-4'>
 					
@@ -21,8 +21,8 @@ const Consulting: React.FC<IConsulting> = ({ content, header, reverse, img }) =>
 
 			</div>
 
-			<div className='flex items-center justify-center square-green bg-white h-40 w-40 scale-150 rounded-full border-medium2'>
-				<img className='inline-block mx-4 w-24 h-24' src={img} alt="" />
+			<div className='flex items-center justify-center square-green bg-white h-40 w-40 scale-150 rounded-full border-medium2 lg:my-0 my-[50px]'>
+				<img className='inline-block mx-4 w-24 h-24 ' src={img} alt="" />
 			</div>
 
 		</div>
