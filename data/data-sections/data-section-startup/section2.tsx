915e001729848e543da12dbf8dbb04cd6d5b2831
JlallyX <nastyj2003@gmail.com>
2024-02-13 17:19:44 +0200

    Update section2.tsx

--- data/data-sections/data-section-startup/section2.tsx
+++ data/data-sections/data-section-startup/section2.tsx
@@ -5,7 +5,7 @@ Accelerator
 </h1>
 
 const content =
-  <div className="text-xl text-center text-teal-700 mb-10">
+  <div className="text-xl text-center mb-10">
 
     <p>Our strategic partnerships with multiple universities offer us the unique access to exceptionally talented graduating software engineers, enriching our project teams and accelerating development. Following a 6-months internship program, you will have the chance to handpick and onboard the most suitable engineers to continue working on your project.</p>
   </div>
