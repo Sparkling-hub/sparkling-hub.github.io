0e45ac30bd515f12324b881d2a7094aec3c0340d
JlallyX <nastyj2003@gmail.com>
2024-02-14 20:19:27 +0200

    Update section2.tsx

--- data/data-sections/data-section-startup/section2.tsx
+++ data/data-sections/data-section-startup/section2.tsx
@@ -1,11 +1,11 @@
 
 const header =
-<h1 className="text-5xl mb-10 mt-14 text-teal-800 text-center">
+<h1 className="text-5xl mb-5   text-primary-light text-center">
 Accelerator
 </h1>
 
 const content =
-  <div className="text-xl text-center mb-10">
+  <div className="text-xl text-center text-white ">
 
     <p>Our strategic partnerships with multiple universities offer us the unique access to exceptionally talented graduating software engineers, enriching our project teams and accelerating development. Following a 6-months internship program, you will have the chance to handpick and onboard the most suitable engineers to continue working on your project.</p>
   </div>
@@ -16,8 +16,8 @@ const element = {
 
   content: content,
   header: header,
+  background: 'bg-primary-dark' ,
 
-  padding_bottom: "pb-40 custom-pb-60",
   index: 2,
 
 
