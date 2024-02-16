66f4df885eab05d8c72dc88f406154efe0779673
JlallyX <nastyj2003@gmail.com>
2024-02-16 14:18:32 +0200

    add toasts

--- components/ui/input-sumbit-component/input-sumbit.tsx
+++ components/ui/input-sumbit-component/input-sumbit.tsx
@@ -1,22 +1,19 @@
 import React, { SyntheticEvent, useState } from 'react';
 import axios from 'axios';
-
+import { Bounce, toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   selectForm,
   selectIsValidEmail,
   setCheck,
   setCheckFormByKey
-
-
 } from '@/store/redusers/FormSliceReduser';
-
 
 interface InputSubmitProps {
   name: string;
   type: string;
   disabled: boolean;
-
   http: string;
 }
 
@@ -26,24 +23,53 @@ const InputSubmit: React.FC<InputSubmitProps> = ({ name, type, disabled, http })
   const dispatch = useDispatch();
 
   const handleSubmit = async (e: SyntheticEvent) => {
-
     e.preventDefault();
+    toast.promise(
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject(new Error('Promise is pending...!')); 
+        }, 2000);
+      }),
+      {
+        pending: 'Sending a message...', 
+      }
+    );
     dispatch(setCheck(selectIsValidEmail(formData.email)));
     if (disabled && selectIsValidEmail(formData.email)) {
       try {
         const response = await axios.post(http, { formData });
         setResult(response.data);
+        toast.success('Form submitted successfully!', {
+          position: "top-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce
+        });
       } catch (error: any) {
-        setResult(<p>{error.message}</p> || <p>An error occurred</p>);
+     
+        toast.error(<p>{error.message ||"An error occurred"}</p>, {
+          position: "top-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce
+        });
       }
-    }
-
-    else {
+    } else {
       const requiredKeys: string[] = ['name', 'email', 'message'];
       requiredKeys
         .filter(key => (formData as any)[key] === '')
         .map(key => { dispatch(setCheckFormByKey({ key: key, value: 'Fill in the following fields:' })) });
-    };
+    }
   }
 
   const buttonClass = disabled && selectIsValidEmail(formData.email) ? 'bg-teal-500' : 'bg-color-primary-dark';
@@ -53,20 +79,27 @@ const InputSubmit: React.FC<InputSubmitProps> = ({ name, type, disabled, http })
       <input
         name={name}
         type={type}
-        className={`no-underline text-white py-3 px-8  rounded-3xl p-2 w-40 m-auto ${buttonClass}`}
+        className={`no-underline text-white py-3 px-8 rounded-3xl p-2 w-40 m-auto ${buttonClass}`}
         onClick={handleSubmit}
       />
       <div className='absolute top-[150%] text-center text-xl w-full font-bold'>
-
-        {typeof result === 'object' || result === '' ? (
-          <h3 className='text-red-500'>{result}</h3>
-        ) : (
-          <h3 className='text-teal-600 '>{result}</h3>
-        )}
+        <ToastContainer
+        className="mt-[5%]"
+          position="top-left"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
       </div>
     </>
   );
 };
 
 export default InputSubmit;
-
