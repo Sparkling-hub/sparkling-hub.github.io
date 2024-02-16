827e8ae7419bae22428237e5b2743fd7b3fcc87c
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2024-02-16 17:21:41 +0000

    fix

--- components/form-contact/form.tsx
+++ components/form-contact/form.tsx
@@ -92,7 +92,7 @@ const Form: React.FC = () => {
             name="submit"
 
             disabled={!!(formData.name && formData.email && formData.message)}
-            http={"https://sparkling-hub.github.io/send-form"}
+            http={"https://sparkling-hub-github-io.vercel.app/send-form"}
           />
         </div>
       </div>
