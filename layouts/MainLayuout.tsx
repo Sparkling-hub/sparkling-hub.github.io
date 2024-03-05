f8810a203e4873572b914b1b2665bf8ea25a1cd4
JlallyX <nastyj2003@gmail.com>
2024-03-05 20:49:37 +0200

    add logo

--- layouts/MainLayuout.tsx
+++ layouts/MainLayuout.tsx
@@ -2,6 +2,7 @@
 import React, { ReactNode } from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import Head from 'next/head';
 
 interface MainLayoutProps {
 	children: ReactNode;
@@ -12,6 +13,10 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 	return (
 
 		<div className='w-full min-w-[400px] font-Roboto'>
+			 <Head>
+        <link rel="icon" href="/S.svg" />
+  
+      </Head>
 			<Navbar />
 			<main>{children}</main>
 			<Footer />
