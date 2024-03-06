75f81e139b710f54ae2fa23ccd8e3c1e64acab5a
JlallyX <nastyj2003@gmail.com>
2024-03-07 01:28:08 +0200

    fix contact header

--- data/data-sections/data-section-contact/section_one.tsx
+++ data/data-sections/data-section-contact/section_one.tsx
@@ -1,14 +1,14 @@
 const header = (
- <h1 className="text-5xl my-4 text-primary-darkTeal font-bold"> Contact Us
+ <h1 className="text-5xl my-2 text-primary-darkTeal font-bold"> Contact Us
   </h1>
 );
 
 const content = (
-  <p className="text-xl text-primary-darkTeal">
-    <strong className="text-2xl">Get in touch!</strong>
-    <br />
-    Drop us a message to learn more about our innovative solutions.
-  </p>
+  <div className="text-2xl text-primary-darkTeal leading-[40px]">
+    <h2 className="font-bold">Get in touch!</h2>
+
+    <h2 className="leading-[30px] font-bold">Drop us a message to learn more about our innovative solutions.</h2> 
+  </div>
 );
 
 const image = <img src="/img/section/ContactUs.png" className="" alt="SectionOne"/>;
