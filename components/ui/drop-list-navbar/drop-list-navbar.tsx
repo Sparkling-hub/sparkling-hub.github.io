5643a9642cc82f491fa5f95499e64fe3e599515f
JlallyX <nastyj2003@gmail.com>
2024-03-08 05:18:42 +0200

    fix safari

--- components/ui/drop-list-navbar/drop-list-navbar.tsx
+++ components/ui/drop-list-navbar/drop-list-navbar.tsx
@@ -80,7 +80,8 @@ const DisabledSelect: React.FC<IDropList> = ({ name, DataLink, mobileMenuOpen })
                     before:left-[43%]
                     before:border-l-[18px]
                     before:drop-shadow-[0_-3px_3px_rgba(0,0,0,0.0.1)]
-                    before:top-[-15px]"
+                    before:top-[-15px]
+                    before:safari-specific"
               >
                 {DataLink.map((service, index) => (
                   <Link key={service.id} className="p-3 m-2 transform transition-transform hover:scale-110 " href={service.href}>
