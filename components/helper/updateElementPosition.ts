c3827d291a69b96f68ea2c92e7b1c9796039b082
JlallyX <nastyj2003@gmail.com>
2024-03-11 11:04:19 +0200

    asf

--- components/helper/updateElementPosition.ts
+++ components/helper/updateElementPosition.ts
@@ -1,15 +1,17 @@
-export const updateElementPosition = (activePoint: string | null) : any => {
-    if (!activePoint) return null;
-    const svgPoint = document.getElementById(activePoint);
-    const parentElement = svgPoint?.parentElement;
+export const updateElementPosition = (activePoint: string | null): any => {
+  if (!activePoint) return null;
+  const svgPoint = document.getElementById(activePoint);
+  const parentElement = svgPoint?.parentElement;
+
+  if (svgPoint && parentElement) {
+    const svgPointRect = svgPoint.getBoundingClientRect();
+    const parentRect = parentElement.getBoundingClientRect();
+
+    const relativeHeight = svgPointRect.top - parentRect.top + parentElement.scrollTop;
+    const relativeWidth = svgPointRect.left - parentRect.left + parentElement.scrollLeft;
+
+    return [relativeHeight, relativeWidth];
+  }
   
-    if (svgPoint && parentElement) {
-      const rect = svgPoint.getBoundingClientRect();
-      const parentRect = parentElement.getBoundingClientRect();
-  
-      const relativeHeight = rect.top - parentRect.top - parentElement.scrollTop;
-      const relativeWidth = rect.left - parentRect.left - parentElement.scrollLeft;
-  
-      return ([relativeHeight, relativeWidth]);
-    }
-  };
+  return null;
+};
