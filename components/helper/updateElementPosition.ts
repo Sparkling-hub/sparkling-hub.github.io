2950224e111fd9eb934cfb5f7eb7e2f838bd9e1c
JlallyX <nastyj2003@gmail.com>
2024-03-11 11:22:40 +0200

    Update updateElementPosition.ts

--- components/helper/updateElementPosition.ts
+++ components/helper/updateElementPosition.ts
@@ -5,12 +5,8 @@ export const updateElementPosition = (activePoint: string | null): any => {
 
   if (svgPoint && parentElement) {
 
-    const svgPointRect = svgPoint.getBoundingClientRect();
-    const parentRect = parentElement.getBoundingClientRect();
-    console.log(svgPointRect)
-    console.log(parentRect)
-    const relativeHeight = svgPointRect.top - parentRect.top + parentElement.scrollTop;
-    const relativeWidth = svgPointRect.left - parentRect.left + parentElement.scrollLeft;
+    const relativeHeight = svgPoint.offsetTop;
+    const relativeWidth = svgPoint.offsetLeft;
 
     return [relativeHeight, relativeWidth];
   }
