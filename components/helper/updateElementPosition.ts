5561613cac92c3416cbcecee09e6a111cb3277fa
JlallyX <nastyj2003@gmail.com>
2024-03-11 11:40:04 +0200

    Update updateElementPosition.ts

--- components/helper/updateElementPosition.ts
+++ components/helper/updateElementPosition.ts
@@ -2,18 +2,18 @@ export const updateElementPosition = (activePoint: string | null): any => {
   if (!activePoint) return null;
   const svgPoint = document.getElementById(activePoint);
   const parentElement = svgPoint?.parentElement;
-
   if (svgPoint && parentElement) {
     let relativeHeight, relativeWidth;
 
     // Проверяем, является ли текущий браузер Safari
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
+   
     if (isSafari) {
       // Используем альтернативный метод для Safari
-      relativeHeight = svgPoint.offsetTop;
-      relativeWidth = svgPoint.offsetLeft;
-    } else {
+      relativeHeight = svgPoint.offsetTop - parentElement.offsetTop;
+      relativeWidth = svgPoint.offsetLeft - parentElement.offsetLeft;
+    }else {
       // Используем стандартный метод для других браузеров
       const svgPointRect = svgPoint.getBoundingClientRect();
       const parentRect = parentElement.getBoundingClientRect();
