3e340cb1d419656d48eb8acf3a69d545bf19d6e0
JlallyX <nastyj2003@gmail.com>
2024-03-11 18:44:48 +0200

    fix

--- components/helper/updateElementPosition.ts
+++ components/helper/updateElementPosition.ts
@@ -14,10 +14,7 @@ export const updateElementPosition = (activePoint: string | null): any => {
  
       relativeHeight = svgPointRect.top - parentRect.top + parentElement.scrollTop;
       relativeWidth = svgPointRect.left - parentRect.left + parentElement.scrollLeft;
-   
-      console.log(relativeHeight)
-    
-      console.log(relativeWidth)
+
       svgPoint.classList.add('hidden')
     return [relativeHeight, relativeWidth];
   }
