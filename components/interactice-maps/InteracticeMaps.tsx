279334b4c89109b49a1e37bf275ccbbe298db1e6
JlallyX <nastyj2003@gmail.com>
2024-03-12 09:54:56 +0200

    Update InteracticeMaps.tsx

--- components/interactice-maps/InteracticeMaps.tsx
+++ components/interactice-maps/InteracticeMaps.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
 	selectMaps,
-	setCurrentMap,
 	setHovered,
 	setLastHovered,
 	setActiveOfficePoint,
@@ -16,56 +15,46 @@ import { updateElementPosition } from "../helper/updateElementPosition";
 const InteracticeMaps: React.FC = () => {
 	const dispatch = useDispatch();
 	const [hoverTimer, setHoverTimer] = useState<NodeJS.Timeout | null>(null);
-	const { currentMap, hovered, lastHovered, activeOfficePoint } = useSelector(selectMaps);
+	const { currentMap, hovered, lastHovered } = useSelector(selectMaps);
 
 	useEffect(() => {
-		
 		let points = document.querySelectorAll('.office_point');
 
 		setupPointEventListeners(points);
-
-		
 		handleHover();
-	
 
 		if (lastHovered && hovered !== lastHovered || (lastHovered && !hovered)) {
 			let elements = document.getElementById(lastHovered)?.children;
-	
+
 			if (elements) {
-				for (const element of elements) {
-					element.classList.remove('scale-125');
-			
-				
-			
-				}
+				for (const element of elements) {element.classList.remove('scale-125');}
 			}
 		}
 
-	
-
 		return () => {
 			removePointEventListeners(points);
-		
 		};
+
 	}, [currentMap, dispatch]);
+
 
 	const handleHover = () => {
 		let points = document.querySelectorAll('.office_point');
 		if (!points.length) return;
-	
+
 		if (hovered) {
 			let elements = document.getElementById(hovered)?.querySelectorAll('svg');
-	
+
 			if (elements && !lastHovered) {
 				for (const element of elements) {
 					element.classList.add('svg-scale');
-;
 					dispatch(setLastHovered(hovered));
 				}
 			}
 		} else {
+
 			let lastHoveredElement = document.getElementById(lastHovered)?.querySelectorAll('svg');
-	
+
 			if (lastHoveredElement) {
 				for (const element of lastHoveredElement) {
 					element.classList.remove('svg-scale');
@@ -74,16 +63,10 @@ const InteracticeMaps: React.FC = () => {
 			}
 		}
 	};
-	
-	
-	
-	
-	
-	
+
 
 	const setupPointEventListeners = (points: NodeListOf<Element>) => {
 		points.forEach((element) => {
-	
 			element.addEventListener('mouseover', handleHoverOver);
 			element.addEventListener('mouseout', handleHoverOut);
 		});
@@ -91,60 +74,41 @@ const InteracticeMaps: React.FC = () => {
 
 	const removePointEventListeners = (points: NodeListOf<Element>) => {
 		points.forEach((element) => {
-		
 			element.addEventListener('mouseover', handleHoverOver);
 			element.addEventListener('mouseout', handleHoverOut);
 		});
 	};
 
 
-
-	const handlePointsClick = (e: any) => {		
-		let officeId = e.currentTarget.id;
-		let points = document.querySelectorAll('.office_point');
-	
-
-
-		for (const element of points) {
-			element.classList.remove('hidden');
-		}
-
-		dispatch(setActiveOfficePointCoords(updateElementPosition(officeId)));
-		dispatch(setActiveOfficePoint(officeId));
-		
-	};
 	const handleHoverOver = (e: any) => {
 		let points = document.querySelectorAll('.office_point');
-		const object = e.currentTarget;
 		const officeId = e.currentTarget.id;
-	  
+
 		const newTimer = setTimeout(() => {
 			for (const element of points) {
 				element.classList.remove('hidden');
 			}
-	
-	  
-		  dispatch(setActiveOfficePointCoords(updateElementPosition(officeId)));
-		  dispatch(setActiveOfficePoint(officeId));
+			dispatch(setActiveOfficePointCoords(updateElementPosition(officeId)));
+			dispatch(setActiveOfficePoint(officeId));
 		}, 150);
-	  
+
 		setHoverTimer(newTimer);
-	  
+
 		e.stopPropagation();
-	  };
-	  
-	  const handleHoverOut = (e: any) => {
+	};
+
+	const handleHoverOut = (e: any) => {
 		if (hoverTimer) {
-		  clearTimeout(hoverTimer);
-		  setHoverTimer(null);
+			clearTimeout(hoverTimer);
+			setHoverTimer(null);
 		}
 		dispatch(setHovered(null));
-	  };
-	  
+	};
 
 
-	  
-	  
+
+
+
 	return (
 		<div className="flex-section block_map relative  max-w-screen-2xl ">
 			<div className="absolute inset-0 z-behind"></div>
