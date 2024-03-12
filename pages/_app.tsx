137686f2ad77198949d9a3dffd86b3b00567a364
JlallyX <nastyj2003@gmail.com>
2024-03-12 14:47:28 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,10 +14,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<Provider store={store}>
 			<Head>
 				<title>Sparkling</title>
+				<meta name="author" content="Sparkling"/>
+
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta http-equiv="Content-Language" content="en" />
 				<meta name="description" content="Sparkling Co. is an industry expert in digital services with over a decade of field experience in Software Development. Our expertise extends to providing dedicated teams of developers and project managers for both in-house project as well as in Team Augmentation, where our experts seamlessly integrate into client-led endeavours. Headquartered in Switzerland our branches reach out across Europe and North Africa, from London to Tunis." />
-				<meta name="keywords" content="web development, programming, frontend, backend, website, careers, work" />
+				<meta name="keywords" content="web development, sparkling, programming, frontend, backend, website, careers, work" />
 				<link rel="icon" href="/S.svg" />
 				<meta property="og:image" content="/S.svg" />
 
