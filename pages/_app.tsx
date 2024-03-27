ab3ad64cec23c2bf02e0fc25a80d89a925c19188
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-03-27 11:43:31 +0200

    fix (#148)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,21 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<Provider store={store}>
 			<Head>
 				<title>Sparkling</title>
-				<meta name="author" content="Sparkling"/>
+				<meta name="author" content="Sparkling" />
+				<meta property="og:title" content="Sparkling,Co" />
+				<meta property="og:image" content="/S.svg" />
+				<meta property="og:url" content="https://www.sparkling.co.com/" />
+				<meta property="og:type" content="website" />
+				<meta property="og:description" content="Sparkling Co. is an industry expert in digital services with over a decade of field experience in Software Development. Our expertise extends to providing dedicated teams of developers and project managers for both in-house project as well as in Team Augmentation, where our experts seamlessly integrate into client-led endeavours. Headquartered in Switzerland our branches reach out across Europe and North Africa, from London to Tunis." />
+				<meta name="keywords" content="web development, sparkling, programming, frontend, backend, website, careers, work" />
 
+				<meta property="twitter:title" content="Sparkling.Co" />
+				<meta property="twitter:image" content="/S.svg" />
+				<meta property="twitter:url" content="https://www.sparkling.co.com/" />
+				<meta property="twitter:type" content="website" />
+				<meta property="og:description" content="Sparkling Co. is an industry expert in digital services with over a decade of field experience in Software Development. Our expertise extends to providing dedicated teams of developers and project managers for both in-house project as well as in Team Augmentation, where our experts seamlessly integrate into client-led endeavours. Headquartered in Switzerland our branches reach out across Europe and North Africa, from London to Tunis." />
+				
+				<meta name="keywords" content="web development, sparkling, programming, frontend, backend, website, careers, work" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta http-equiv="Content-Language" content="en" />
 				<meta name="description" content="Sparkling Co. is an industry expert in digital services with over a decade of field experience in Software Development. Our expertise extends to providing dedicated teams of developers and project managers for both in-house project as well as in Team Augmentation, where our experts seamlessly integrate into client-led endeavours. Headquartered in Switzerland our branches reach out across Europe and North Africa, from London to Tunis." />
